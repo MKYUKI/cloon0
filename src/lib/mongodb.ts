2091ed929a3d345bf43fa877fa7bb8be3eadb314
Masaki Kusaka <masaki.kusaka@example.com>
2024-12-24 22:48:46 +0900

    Fix build errors and update dependencies

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -13,19 +13,13 @@ let clientPromise: Promise<MongoClient>;
 if (process.env.NODE_ENV === 'development') {
   // In development, use a global variable to preserve the connection across module reloads
   if (!(global as any)._mongoClientPromise) {
-    client = new MongoClient(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    client = new MongoClient(uri);
     (global as any)._mongoClientPromise = client.connect();
   }
   clientPromise = (global as any)._mongoClientPromise;
 } else {
   // In production, don't use a global variable
-  client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  client = new MongoClient(uri);
   clientPromise = client.connect();
 }
 
