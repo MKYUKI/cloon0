a2885e6412c0335905164a4baa9df9168893e369
Masaki Kusaka <masaki.kusaka@example.com>
2024-12-31 09:23:23 +0900

    Fix: Resolve ESLint warning, port issues, and update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
-    },
-  };
-  
-  module.exports = nextConfig;
+  reactStrictMode: true,
+  images: {
+    domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
+  },
+};
+
+module.exports = nextConfig;
