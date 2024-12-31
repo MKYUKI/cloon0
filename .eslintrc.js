4eedf8b3448e8997a67ead801cb12f45448f704a
Masaki Kusaka <masaki.kusaka@example.com>
2024-12-31 13:55:20 +0900

    Fix Link component usage and integrate Gemini API

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,12 @@
+// .eslintrc.js
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-  env: {
-    browser: true,
-    amd: true,
-    node: true,
-  },
-  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
-    'next/core-web-vitals',
+    "next",
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
   ],
   rules: {
-    'prettier/prettier': 'error',
-    'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    'react/prop-types': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
+    // カスタムルール
   },
 };
